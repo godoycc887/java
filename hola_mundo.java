ad7eab06b8c70f1d7c1425a5227fc87d289eefd0
godoycc887 <91565854+godoycc887@users.noreply.github.com>
2021-10-06 14:22:09 -0500

    hola mundo en java

---
+++ hola_mundo.java
@@ -0,0 +1,15 @@
+public class hola_mundo{
+           /**
+           *
+           *Aplicacion java
+           *titele:           Hola mundo en JAVA
+           *Descrition:       hola mundo en JAVA desde vim en windows
+           *Author:           Carlos Alberto Godoy Cruz
+           *Date              20211006
+           *Version:          1
+           *Usage:            C:\Users\ACER-PC\Documents
+           */
+                         public static void main(String []args){
+                                System.out.println("Hola Mundo");//prints hello world
+           }
+}
